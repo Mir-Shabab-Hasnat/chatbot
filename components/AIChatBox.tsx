16860b42eab329ef60c83fa7defd03bd3d58845d
unknown <shababhasnat@gmail.com>
2024-10-14 12:12:40 -0600

    chatbox shell created

--- components/AIChatBox.tsx
+++ components/AIChatBox.tsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import {useChat} from "ai/react"
-import { cn } from '@/lib/utils'
+import React from "react";
+import { useChat } from "ai/react";
+import { cn } from "@/lib/utils";
+import { XCircle } from "lucide-react";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
 
 interface AIChatBoxProps {
-    open: boolean
-    onClose: () => void
+  open: boolean;
+  onClose: () => void;
 }
 
-const AIChatBox = ({open, onClose} : AIChatBoxProps) => {
+const AIChatBox = ({ open, onClose }: AIChatBoxProps) => {
   const {
     messages,
     input,
@@ -15,18 +18,34 @@ const AIChatBox = ({open, onClose} : AIChatBoxProps) => {
     handleSubmit,
     setMessages,
     isLoading,
-    error
-  } = useChat()
+    error,
+  } = useChat();
 
   return (
-    <div className={cn('bottom-0 right-0 z-10 w-full max-w-[500px] p-1 xl:right-36', 
+    <div
+      className={cn(
+        "bottom-0 right-0 z-10 w-full max-w-[500px] p-1 xl:right-36",
         open ? "fixed" : "hidden"
-    )}>
-        ChatBox
+      )}
+    >
+      <button onClick={onClose} className="mb-1 ms-auto block">
+        <XCircle size={30} />
+      </button>
+      <div className="flex h-[600px] flex-col rounded bg-background border shadow-xl">
+        <div className="h-full">Messages</div>
+        <form onSubmit={handleSubmit} className="m-3 flex gap-1">
+          <Input 
+            value={input}
+            onChange={handleInputChange}
+            placeholder="Say someting..."
+          />
+          <Button type="submit">
+            Send
+          </Button>
+        </form>
+      </div>
     </div>
-)
+  );
+};
 
-}
-
-
-export default AIChatBox
+export default AIChatBox;
